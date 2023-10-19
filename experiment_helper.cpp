6c791bab8aa1fa1721428906b9af7e65fbffd226
LordStarkie <s3902159@student.rmit.edu.au>
2023-10-20 02:55:56 +1100

    added used size

--- experiment_helper.cpp
+++ experiment_helper.cpp
@@ -1,19 +1,17 @@
 #include "mem_manager.h"
 #include "experiment_helper.h"
 
-double calculate_internal_fragmentation() {
-    double total_allocated = 0; 
-    double total_used = 0;     
+double calculate_internal_fragmentation(const std::forward_list<memory_chunk>& occupied_chunks) {
+    double total_fragmentation = 0;
 
     for (const auto& chunk : occupied_chunks) {
-        total_allocated += chunk.size;
-        total_used += chunk.used_size;
+        total_fragmentation += chunk.size - chunk.used_size;
     }
 
-    return total_allocated - total_used;
+    return total_fragmentation;
 }
 
-double calculate_external_fragmentation() {
+double calculate_external_fragmentation(const std::forward_list<memory_chunk>& free_chunks) {
     double total_free_memory = 0;
 
     for (const auto& chunk : free_chunks) {
@@ -23,12 +21,22 @@ double calculate_external_fragmentation() {
     return total_free_memory;
 }
 
-// helper function to calculate fragmentation percentages, runs both functions
-std::pair<double, double> calculate_fragmentation_percentages() {
-    double internal_fragmentation = calculate_internal_fragmentation();
-    double external_fragmentation = calculate_external_fragmentation();
-    double total_memory = internal_fragmentation + external_fragmentation;
+// runs both other functions
+std::pair<double, double> calculate_fragmentation_percentages(const std::forward_list<memory_chunk>& occupied_chunks, const std::forward_list<memory_chunk>& free_chunks) {
+    // sum of chunks
+    double total_memory = 0; 
+    for (const auto& chunk : occupied_chunks) {
+        total_memory += chunk.size;
+    }
+    for (const auto& chunk : free_chunks) {
+        total_memory += chunk.size;
+    }
 
-    return std::make_pair((internal_fragmentation / total_memory) * 100,
-                          (external_fragmentation / total_memory) * 100);
+    double internal_frag = calculate_internal_fragmentation(occupied_chunks);
+    double external_frag = calculate_external_fragmentation(free_chunks);
+
+    double internal_percentage = (internal_frag / total_memory) * 100;
+    double external_percentage = (external_frag / total_memory) * 100;
+
+    return { internal_percentage, external_percentage };
 }
